a012e25df578c786862434deec22672820df1f6a
yk2kus <yogeshkushwaha4@gmail.com>
2018-04-23 19:47:25 -0300

    FIX object for field analytic_account_id in contrACT LINE

--- contract/models/__init__.py
+++ contract/models/__init__.py
@@ -4,5 +4,6 @@
 from . import account_analytic_contract
 from . import account_analytic_account
 from . import account_analytic_invoice_line
-from . import account_analytic_contract_line
+# changing object which is not requried
+# from . import account_analytic_contract_line
 from . import account_invoice
